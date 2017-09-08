105ee7f872bfbbf8067de9c433c3179ee81fa262
magcode <marko.web1@outlook.com>
2017-09-08 18:06:29 +0200

    install script

---
+++ nocontroller.sh
@@ -0,0 +1,5 @@
+sed 's/pkill -9 mcad/exit 0\n        pkill -9 mcad/;s/pkill -9 wpa_supplicant/exit 0\n        pkill -9 wpa_supplicant/' /usr/etc/syswrapper.sh > /etc/persistent/syswrapper.sh
+chmod 755 /etc/persistent/syswrapper.sh
+wget --no-check-certificate https://raw.githubusercontent.com/magcode/mpower-tools/master/rc.poststart -O /etc/persistent/rc.poststart
+chmod 755 /etc/persistent/rc.poststart
+save
