418bf86959ae8bd42a0ba7c682c6ee7770e0ba3c
magcode <marko.web1@outlook.com>
2017-09-08 18:23:19 +0200

    save must be done manually

--- nocontroller.sh
+++ nocontroller.sh
@@ -5,4 +5,3 @@ sed 's/pkill -9 mcad/exit 0\n        pkill -9 mcad/;s/pkill -9 wpa_supplicant/ex
 chmod 755 /etc/persistent/syswrapper.sh
 wget --no-check-certificate https://raw.githubusercontent.com/magcode/mpower-tools/master/rc.poststart -O /etc/persistent/rc.poststart
 chmod 755 /etc/persistent/rc.poststart
-save
