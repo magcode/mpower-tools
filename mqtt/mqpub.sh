0251b9b195c2937a96ac75df0eb787cf728495a2
magcode <marko.web1@outlook.com>
2017-09-25 21:17:56 +0200

    poc native mqtt on mpower

---
+++ mqtt/mqpub.sh
@@ -0,0 +1,7 @@
+export LD_LIBRARY_PATH=/var/etc/persistent
+while sleep 10; 
+do 
+    cat /proc/power/active_pwr2 | ./mosquitto_pub -h 192.168.155.20 -t home/mpower1/2/activepwr -s
+    cat /proc/power/cf_count2 | ./mosquitto_pub -h 192.168.155.20 -t home/mpower1/2/cfcount -s
+    /sbin/cgi /usr/www/mfi/sensors.cgi | tail -n +3 | ./mosquitto_pub -h 192.168.155.20 -t home/mpower1/sensors -s
+done
