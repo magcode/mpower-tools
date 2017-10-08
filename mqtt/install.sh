6e18ca3aca9af0322b3aa0634918ea1fbf81cdf0
magcode <marko.web1@outlook.com>
2017-10-08 15:15:33 +0200

    install

--- mqtt/install.sh
+++ mqtt/install.sh
@@ -7,7 +7,6 @@ wget --no-check-certificate https://github.com/magcode/mpower-tools/blob/master/
 wget --no-check-certificate https://raw.githubusercontent.com/magcode/mpower-tools/master/mqtt/mqpub.sh -O /var/etc/persistent/mqtt/mqpub.sh
 wget --no-check-certificate https://raw.githubusercontent.com/magcode/mpower-tools/master/mqtt/mqsub.sh -O /var/etc/persistent/mqtt/mqsub.sh
 
-
 chmod 755 /var/etc/persistent/mqtt/mosquitto_pub
 chmod 755 /var/etc/persistent/mqtt/mosquitto_sub
 chmod 755 /var/etc/persistent/mqtt/mqpub.sh
