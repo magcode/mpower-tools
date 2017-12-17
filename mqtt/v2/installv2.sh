0a206ef2539353e46beca72deb269467fea307f2
magcode <marko.web1@outlook.com>
2017-12-17 14:21:51 +0100

    v2

--- mqtt/v2/installv2.sh
+++ mqtt/v2/installv2.sh
@@ -8,7 +8,7 @@ echo "Installing mPower MQTT v2 ..."
 wget --no-check-certificate -q $BASEURL/libmosquitto.so.1?raw=true -O $LOCALDIR/libmosquitto.so.1
 wget --no-check-certificate -q $BASEURL/mosquitto_pub?raw=true -O $LOCALDIR/mosquitto_pub
 wget --no-check-certificate -q $BASEURL/mosquitto_sub?raw=true -O $LOCALDIR/mosquitto_sub
-mkdir $LOCALSCRIPTDIR
+mkdir -p $LOCALSCRIPTDIR
 rm $LOCALDIR/*
 wget --no-check-certificate -q $BASEURL/v2/mqrunv2.sh -O $LOCALSCRIPTDIR/mqrunv2.sh
 wget --no-check-certificate -q $BASEURL/v2/mqpubv2-static.sh -O $LOCALSCRIPTDIR/mqpubv2-static.sh
@@ -17,8 +17,8 @@ wget --no-check-certificate -q $BASEURL/v2/mpower-pub.cfg -O $LOCALSCRIPTDIR/mpo
 
 #wget --no-check-certificate -q https://raw.githubusercontent.com/magcode/mpower-tools/master/mqtt/mqsub.sh -O /var/etc/persistent/mqtt/mqsub.sh
 
-chmod 755 /var/etc/persistent/mqtt/mosquitto_pub
-chmod 755 /var/etc/persistent/mqtt/mosquitto_sub
+chmod 755 $LOCALDIR/mosquitto_pub
+chmod 755 $LOCALDIR/mosquitto_sub
 chmod 755 $LOCALSCRIPTDIR/mqrunv2.sh
 chmod 755 $LOCALSCRIPTDIR/mqpubv2-static.sh
 chmod 755 $LOCALSCRIPTDIR/mqpubv2.sh
