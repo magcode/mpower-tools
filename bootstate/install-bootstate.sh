fe51887359bd3bef59df909ec8f66204cea2542f
magcode <marko.web1@outlook.com>
2018-08-09 21:59:31 +0200

    more fixes

--- bootstate/install-bootstate.sh
+++ bootstate/install-bootstate.sh
@@ -5,11 +5,13 @@ echo "Installing bootstate ..."
 scriptdir=/var/etc/persistent/bootstate
 
 mkdir -p $scriptdir
-config=$scriptdir/bootstate.cfg
-applyscript="sleep 60; $scriptdir/apply-bootstate.sh"
+config="$scriptdir/bootstate.cfg"
+applyscript="$scriptdir/apply-bootstate.sh"
+poststartscript="sleep 60; $applyscript"
 poststart=/etc/persistent/rc.poststart
 
-wget --no-check-certificate -q https://raw.githubusercontent.com/magcode/mpower-tools/master/bootstate/apply-bootstate.sh -O $scriptdir/apply-bootstate.sh
+wget --no-check-certificate -q https://raw.githubusercontent.com/magcode/mpower-tools/master/bootstate/apply-bootstate.sh -O $applyscript
+chmod 775 $applyscript
 
 if [ ! -f $config ]; then
     echo "$config not found, creating it ..."
@@ -28,13 +30,13 @@ if [ ! -f $poststart ]; then
     chmod 755 $poststart
 fi
 
-if grep -q "$applyscript" "$poststart"; then
+if grep -q "$poststartscript" "$poststart"; then
    echo "Found $poststart entry. File will not be changed"
 else
    echo "Adding start command to $poststart"
-   echo "$applyscript" >> $poststart
+   echo "$poststartscript" >> $poststart
 fi
 
 echo "Done!"
 cfgmtd -w -p /etc/
-echo "Adapt your settings in '$config' and call '/var/etc/persistent/bootstate/apply-bootstate.sh' afterwards."
+echo "Adapt your settings in '$config' and call '$applyscript' afterwards."
