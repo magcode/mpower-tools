677f15651006a2c7c7b56ab1da7ffbfa6f391ac9
magcode <marko.web1@outlook.com>
2025-02-07 20:46:14 +0100

    print start process

--- nocontroller/start.sh
+++ nocontroller/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#
 
+echo "nocontroller starts"
 mount --bind /var/etc/persistent/nocontroller/syswrapper.sh /usr/etc/syswrapper.sh
 sed -e 's/mcad/mcad -d/' /etc/inittab > /tmp/inittab && mv /tmp/inittab /etc/inittab
 kill -HUP 1
